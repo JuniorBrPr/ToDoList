42f8422b6e4f39a2ee8bc66348b56f09f5f98c77
Junior Brito <junior.br.pr@gmail.com>
2023-02-20 21:40:30 +0100

    Final stage

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
-const taskValue = document.getElementById("input-task");
+const newTaskText = document.getElementById("input-task");
 const taskList = document.getElementById("task-list");
 const addTaskBtn = document.getElementById("add-task-button");
+let taskListArray = [];
 
 addTaskBtn.addEventListener("click", addTask);
-loadCheckboxes();
+document.addEventListener("DOMContentLoaded", loadTaskList);
+document.addEventListener("DOMContentLoaded", saveTasks);
 
-function loadCheckboxes() {
-    document.querySelectorAll(".completed-check").forEach((checkbox) => {
-        checkbox.addEventListener("click", taskCompleted);
-    });
+function loadTaskList() {
+    taskListArray = JSON.parse(localStorage.getItem("tasks"));
+    if (taskListArray !== null) {
+        taskListArray.forEach((task) => {
+            createTask(task.text, task.completed);
+        });
+    } else {
+        taskListArray = [];
+    }
 }
 
 function addTask() {
-    const taskLi = createTask();
-    taskList.appendChild(taskLi);
-    taskValue.value = "";
+    createTask(newTaskText.value, false);
+    taskListArray.push(
+        {
+            text: newTaskText.value,
+            completed: false
+        }
+    );
+    newTaskText.value = "";
+    saveTasks();
 }
 
-function createTask() {
+function createTask(taskText, completed) {
     const checkbox = document.createElement("input");
     checkbox.className = "completed-check"
     checkbox.type = "checkbox";
     checkbox.name = "completed";
     checkbox.value = "completed";
+    checkbox.checked = completed;
+
     checkbox.addEventListener("click", taskCompleted);
     const text = document.createElement("span");
-    text.innerText = taskValue.value;
+    text.innerText = taskText;
     text.className = "task";
     const deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "x";
+    deleteBtn.innerText = "X";
     deleteBtn.className = "delete-btn";
     deleteBtn.type = "button";
     deleteBtn.name = "delete";
@@ -39,7 +54,10 @@ function createTask() {
     li.appendChild(checkbox);
     li.appendChild(text);
     li.appendChild(deleteBtn);
-    return li;
+    if (completed) {
+        checkbox.parentNode.querySelector(".task").classList.add("completed");
+    }
+    taskList.appendChild(li);
 }
 
 function taskCompleted() {
@@ -49,8 +67,23 @@ function taskCompleted() {
     } else {
         task.classList.remove("completed");
     }
+    setTaskStatus(task.innerText)
+}
+
+function setTaskStatus(taskText) {
+    const index = taskListArray.findIndex((task) => task.text === taskText);
+    taskListArray[index].completed = !taskListArray[index].completed;
+    saveTasks()
 }
 
 function deleteTask() {
     this.parentNode.remove();
+    delete taskListArray[taskListArray.findIndex(
+        (task) => task.text === this.parentNode.querySelector(".task").innerText)
+        ];
+    saveTasks();
+}
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(taskListArray));
 }
